af0684e66636d685f20249d09504d1d0f99a5705
Jaaon <jingxuanshan@hotmail.com>
2026-02-28 08:54:19 -0800

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,1 +1,6 @@
-<?php echo $_SERVER['EVE_CLIENT_SECRET'] ?>
+<?php
+// Check all possible locations
+echo "1. getenv: " . getenv('EVE_CLIENT_SECRET') . "<br>";
+echo "2. $_SERVER: " . ($_SERVER['EVE_CLIENT_SECRET'] ?? 'Not found') . "<br>";
+echo "3. $_ENV: " . ($_ENV['EVE_CLIENT_SECRET'] ?? 'Not found') . "<br>";
+?>
